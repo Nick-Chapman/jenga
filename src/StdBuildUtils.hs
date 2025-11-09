2fb4255bf764e8853e84036004e173ac522f0183
Nick Chapman <nic@amadido.co.uk>
2025-11-09 14:38:29 +0000

    better handling of ..

--- src/StdBuildUtils.hs
+++ src/StdBuildUtils.hs
@@ -8,6 +8,7 @@ module StdBuildUtils
 
 import Interface (Key(..),Loc(..))
 import System.FilePath qualified as FP
+import System.Path.NameManip (guess_dotdot)
 
 mkKey :: Loc -> String -> Key
 mkKey (Loc fp) suffix = Key (Loc (fp++suffix))
@@ -23,9 +24,12 @@ dirLoc (Loc fp) = Loc (FP.takeDirectory fp)
 
 (</>) :: Loc -> String -> Loc
 (</>) (Loc dir) filename0 =
-  Loc (FP.normalise (dir FP.</> filename))
+  Loc (FP.normalise $ removeDotdotIsPossible (dir FP.</> filename))
   where
     filename =
       case filename0 of
         '/':afterLeadingSlash -> afterLeadingSlash
         rel -> rel
+
+removeDotdotIsPossible :: String -> String
+removeDotdotIsPossible s = case guess_dotdot s of Just s -> s; Nothing -> s

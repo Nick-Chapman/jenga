87eba46b4f3667cd3cfae9a360e598cbd2b1bd03
Nick Chapman <nic@amadido.co.uk>
2025-12-03 22:04:23 +0000

    self-test: example 08

---
+++ self-test/08-scanner-chain.sh
@@ -0,0 +1,19 @@
+
+# Initial build
+jenga build -a
+
+# Run the executable
+jenga exec main.exe
+
+# Mod-A (change const value)
+rm defs2.h && echo '#define MY_CONST 11' > defs2.h
+jenga exec main.exe
+
+# Mod-B (shorten the chain)
+rm defs.h && echo '#define MY_CONST 12' > defs.h
+jenga exec main.exe
+
+# Mod-C (repoint the chain)
+echo '#define MY_CONST 13' > defs3.h
+echo '#include "defs3.h"' > defs.h
+jenga exec main.exe

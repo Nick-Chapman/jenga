14e67951c9d883fd8edbf2ee12dbcbc2ec0cea3a
Nick Chapman <nic@amadido.co.uk>
2025-12-03 01:52:12 +0000

    note/crash

--- src/Engine.hs
+++ src/Engine.hs
@@ -69,7 +69,7 @@ dropPrefixChecked prefix str =
   if prefix == str then "." else
     case stripPrefix (prefix++"/") str of
       Just suffix -> suffix
-      Nothing -> error (show ("dropPrefixChecked",prefix,str))
+      Nothing -> error (show ("dropPrefixChecked",prefix,str)) -- TODO: comes "jenga build -r --rel"
 
 relppKey :: Dir -> Key -> String
 relppKey startDir (Key loc) =

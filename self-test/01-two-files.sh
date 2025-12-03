12b6cf04992227e3c8accf52afdb475c0ba40a16
Nick Chapman <nic@amadido.co.uk>
2025-12-03 18:57:15 +0000

    flesh out self-test example 01

--- self-test/01-two-files.sh
+++ self-test/01-two-files.sh
@@ -1,12 +1,63 @@
 
-# Script of self-test jenga commands to be executed for example 01
+# Rejig files into "example" subdirectory
+mkdir example
+mv build.jenga fib.c main.c example
 
-# Initial build...
+# Build from clean
 jenga build -a
 
-# Exepected zero re-build...
+# Run the built artifact
+jenga exec example/main.exe
+
+# Rebuild after no changes
 jenga build -a
 
-# Remove cache and build again
-rm -rf .cache
+# Update main.c "world->UNIVERSE"; two compile actions run
+sed -i 's/world/UNIVERSE/g' example/main.c
+jenga exec -a example/main.exe
+
+# Reverting to previous state of main.c; no actions run
+sed -i 's/UNIVERSE/world/g' example/main.c
+jenga exec -a example/main.exe
+
+# Whitespace change to main.c; no link action run (early cutoff)
+sed -i 's/int main/int      main/g' example/main.c
 jenga build -a
+
+# Update to link under a different name; new link action run
+sed -i 's/main.exe/RENAMED.exe/' example/build.jenga
+jenga exec -a example/RENAMED.exe
+
+# Relocate the example to a new directory; no actions run
+mv example RELOCATED
+jenga build -a
+
+# Duplicate example dir; twice #targets; but no actions run
+cp -rp RELOCATED ANOTHER
+jenga build -a
+
+# Modify one of the example dirs; minimal rebuild as required
+sed -i 's/fib(10)/fib(20)/g' RELOCATED/main.c
+jenga build -a
+
+# Run the two co-existing executabls
+jenga exec RELOCATED/RENAMED.exe
+jenga exec ANOTHER/RENAMED.exe
+
+# Materalize all targets in ,jenga; no actions run
+jenga build -a --materialize
+find ,jenga
+
+# Remove one directory copy; materalize again in ,jenga
+rm -rf RELOCATED
+jenga build -a --materialize
+find ,jenga
+
+# Run build again without --materialize; ,jenga is removed
+jenga build -a
+find ,jenga
+
+# Mod some more, try -q
+sed -i 's/fib(10)/fib(11)/g' ANOTHER/main.c
+jenga build -q
+jenga exec -a ANOTHER/RENAMED.exe

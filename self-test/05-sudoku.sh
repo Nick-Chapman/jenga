262753c8f1d6ee87ab4ce2c0707cc1be8f2de0da
Nick Chapman <nic@amadido.co.uk>
2025-12-10 11:15:19 +0000

    extend self-test 05 to witess duplicate error report bug

--- self-test/05-sudoku.sh
+++ self-test/05-sudoku.sh
@@ -4,3 +4,16 @@ jenga build -a
 
 # Solve a puzzle
 jenga exec solver.exe puzzle
+
+# Modify haskell code (careful with hard-link)
+cat Sudoku.hs > x
+echo xxx >> x
+mv x Sudoku.hs
+
+# Try build; expect ghc error
+# BUG: the compile error is reported twice!
+# We need -j2 to get the bug; and even then it is not guarenteed. perhaps -j3 is better
+jenga.exe build --cache=. --rel -j3 -a
+
+# On rebuild we correctly get just a single report
+jenga.exe build --cache=. --rel -j3 -a
